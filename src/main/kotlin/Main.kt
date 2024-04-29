92b47ea1a666696e70031fd7498e6f4f92a77870
Nikita Garmoza <garmozanikita@gmail.com>
2024-04-29 20:28:50 +0300

    in single file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,57 +1,31 @@
 import java.lang.NumberFormatException
 import java.util.Scanner
 
-class Archive(val notes: List<String>) {
-}
-
 interface Screen {
-
     fun render()
 }
 
-//abstract class StackableScreen(private val stack: ArrayDeque<Screen>) : Screen {
-//
-//    override fun onClose() {
-//        stack.removeLast()
-//    }
-//}
-
 data class MenuItem(val label: String, val onSelect: () -> Unit)
 
-//class SelectableScreen(
-//        stack: ArrayDeque<Screen>
-//) : StackableScreen(stack) {
-//
-//    val menu: MutableList<MenuItem> = mutableListOf()
-//
-//    init {
-//        menu.add(MenuItem("Выход", ::onClose))
-//    }
-//
-//    override fun render() {
-//        TODO("Not yet implemented")
-//    }
-//}
+class Archive(val name: String, val notes: List<String>)
 
-class ArchiveSelectableScreen(
-        private val stack: ArrayDeque<Screen>
+class ArchiveSelectScreen(
+        stack: ArrayDeque<Screen>,
+        archives: MutableList<Archive>
 ) : Screen {
 
     private val scanner: Scanner = Scanner(System.`in`)
     private val menu: MutableList<MenuItem> = mutableListOf()
 
     init {
-        menu.add(MenuItem("Созадть архив") { println("Архив создан") })
+        menu.add(MenuItem("Созадть архив") { stack.addLast(ArchiveCreateScreen(stack)) })
+        for (archive in archives) {
+            menu.add(MenuItem(archive.name) { stack.addLast(NoteSelectScreen(stack)) })
+        }
         menu.add(MenuItem("Выход") {
             stack.removeLast()
             println("Пока! Пока!")
         })
-    }
-
-    private fun renderMenu() {
-        for ((index, item) in menu.withIndex()) {
-            println("$index. ${item.label}")
-        }
     }
 
     override fun render() {
@@ -70,11 +44,42 @@ class ArchiveSelectableScreen(
             println("Пожалуйста, введите цифру")
         }
     }
+
+    private fun renderMenu() {
+        for ((index, item) in menu.withIndex()) {
+            println("$index. ${item.label}")
+        }
+    }
+}
+
+class ArchiveCreateScreen(
+        stack: ArrayDeque<Screen>
+) : Screen {
+
+    private val scanner: Scanner = Scanner(System.`in`)
+
+    override fun render() {
+        println("Введите название архива")
+        val input: String = scanner.nextLine()
+
+
+    }
+}
+
+class NoteSelectScreen(
+        stack: ArrayDeque<Screen>
+) : Screen {
+
+    override fun render() {
+        TODO("Not yet implemented")
+    }
 }
 
 fun main(args: Array<String>) {
+    val archives: MutableList<Archive> = mutableListOf()
+
     val stack = ArrayDeque<Screen>()
-    stack.add(ArchiveSelectableScreen(stack))
+    stack.add(ArchiveSelectScreen(stack, archives))
 
     while (!stack.isEmpty()) {
         stack.last().render()
